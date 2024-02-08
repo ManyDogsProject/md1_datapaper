9bcb15578f619d3b656556fd760887f2b517323d
Jeffrey R. Stevens <jeffrey.r.stevens@protonmail.com>
2024-02-08 11:54:18 -0600

    Update country map

--- md1_countries.R
+++ md1_countries.R
@@ -6,15 +6,17 @@ world_map <- map_data("world") |>
 
 countries <- world_map |> 
   distinct(region) |> 
-  mutate(present = as.factor(ifelse(region %in% c("USA", "Canada", "UK", "Hungary", "Argentina", "Italy", "Croatia", "Austria", "Poland"), 1, 0)))
+  mutate(present = as.factor(case_when(region %in% c("USA", "Canada", "UK", "Hungary", "Argentina", "Italy", "Croatia", "Poland") ~ 2,
+                                       region == "Austria" ~ 1,
+                                       .default = 0)))
 
 countries |> 
   ggplot(aes(fill = present, map_id = region)) +
   geom_map(map = world_map) +
   # expand_limits(x = world_map$long, y = world_map$lat) +
   lims(x = c(-160, 20), y = c(-50, 120)) +
-  scale_fill_manual(values = c("grey", "#28679C")) +
+  scale_fill_manual(values = c("grey", "#008cff", "#28679C")) +
   theme_void() +
   theme(legend.position = "none",
         aspect.ratio = 1)
-ggsave(here("figures/md1_countries.png"), width = 5, height = 4)
+ggsave(here("md1_countries_orig.tiff"), width = 5, height = 4, dpi = 300)
